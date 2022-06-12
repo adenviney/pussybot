9cb4c4b9bf7ccef39be5af5637b5bdf9cce99516
Aden Viney <adenviney@gmail.com>
2022-06-12 23:55:34 +0100

    Fixed dir issue

--- lib/emergency.py
+++ lib/emergency.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix='$', description="coolowo", intents=intents, h
 @commands.has_permissions(administrator=True)
 async def restore(ctx):
     await ctx.send(embed=discord.Embed(title=f"Emergency mode disabled", description="$emergency to restrict commands again", timestamp=datetime.datetime.utcnow(), color=discord.Color.green()))
-    os.system("python pussybot.py")
+    os.system("python ../pussybot.py")
 
 print('BOT RESTARTED IN EMERGENCY MODE, RUN COMMAND $RESTORE TO RESTORE BACK')            
 with open("../tkn.json") as f: token = json.load(f)["token"]
