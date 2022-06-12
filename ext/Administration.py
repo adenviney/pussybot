5204f89fa01b713d5df4dd1b6581e669d68c80c2
Aden Viney <adenviney@gmail.com>
2022-06-12 05:10:37 +0100

    Removed unecessary code block

--- ext/Administration.py
+++ ext/Administration.py
@@ -65,36 +65,6 @@ class Administration(commands.Cog):
         embed.add_field(name="Reason", value=reason, inline=True)
         embed.set_footer(text=f"Unlocked by {ctx.author}")
         await ctx.reply(embed=embed)
-        
-    # @commands.command(name="rolereact", brief="Send an embed which will allow a user to gain a role off of reacting")
-    # @commands.has_permissions(administrator=True)
-    # #$rr <message> role|emoji role|emoji role|emoji
-    # async def rolereact(self, ctx, *, args):
-    #     #args will look like "React to gain role^348324830928|:fish: 32784023947032974|:fish:"
-    #     if ctx.message.author.guild_permissions.administrator == False:
-    #         embed = discord.Embed(title=f"❌ Could not send role react", description=f"Failed to execute", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
-    #         embed.add_field(name="Error", value="You do not have the permission to manage channels", inline=True)
-    #         embed.set_footer(text=f"Attempted by {ctx.author}")
-    #         await ctx.reply(embed=embed)
-    #         return
-    #     #Split the args into a list
-    #     message = args.split("^")
-    #     spaces = message[1].split(" ")
-        
-    #     #Create the embed
-    #     embed = discord.Embed(title=message[0], color=0x2F3136)
-    #     #Send the embed
-    #     msg = await ctx.reply(embed=embed)
-        
-    #     #loop through all indexes in list and strip them of whitespace
-    #     for i in range(len(spaces)):
-    #         spaces[i] = spaces[i].strip()
-    #         fin = spaces[i].split("|")
-    #         await msg.add_reactions(fin[1])
-        
-        
-        
-        # print(spaces)
 
 def setup(bot): bot.add_cog(Administration(bot))
 print("Administration cog loaded")
