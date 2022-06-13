84f8fa982bc92952c4a6eeca8f01194656db81ae
Aden Viney <adenviney@gmail.com>
2022-06-13 07:30:35 +0100

    Added pay command & large amounts now seperated by comma

--- ext/Economy.py
+++ ext/Economy.py
@@ -11,11 +11,13 @@ try: #Everything
 except mysql.connector.Error as err: 
     print(c.color.FAIL + "[ERROR] " + c.color.END + str(err))
 
-class Economy(commands.Cog):
+def addcomma(amount): 
+    return ("{:,}".format(amount))
+class Economy(commands.Cog):    
     def __init__(self, bot): 
         self.bot = bot
         
-    #Commands: $invest, $buy, $sell, $work, $bal, $shop, $inventory, $use, $stats, $daily, $weekly, $monthly, $yearly, $economy,
+    #Commands: $invest, $buy, $sell, $work, $bal, $shop, $inventory, $use, $stats, $daily, $weekly, $monthly, $yearly, $economy, $pay
         
     @commands.command(name="beg", brief="Beg for coins")
     @commands.cooldown(1, 30, commands.BucketType.user)
@@ -47,7 +49,7 @@ class Economy(commands.Cog):
         else:  
             cursor.execute(f"UPDATE users SET coins = coins + {str(amount)} WHERE id = {str(ctx.author.id)}")
             connect.commit()
-        embed = discord.Embed(title=f"You were given {amount} coins for begging.", color=discord.Color.green())
+        embed = discord.Embed(title=f"You were given `${amount}` coins for begging.", color=discord.Color.green())
         embed.set_footer(text="What a begger you are.")
         await ctx.send(embed=embed)
         
@@ -90,7 +92,7 @@ class Economy(commands.Cog):
         cursor.execute(f"UPDATE users SET coins = coins - {str(amount)} WHERE id = {str(ctx.author.id)}")
         cursor.execute(f"UPDATE users SET bank = bank + {str(amount)} WHERE id = {str(ctx.author.id)}")
         connect.commit()
-        await ctx.send(embed=discord.Embed(title=f"You deposited {amount} coins.", color=discord.Color.green()))
+        await ctx.send(embed=discord.Embed(title=f"You deposited `${addcomma(amount)}` coins.", color=discord.Color.green()))
         
     @commands.command(name="withdraw", brief="Withdraw coins", aliases=["with"])
     async def withdraw(self, ctx, amount: str = 0):
@@ -131,27 +133,110 @@ class Economy(commands.Cog):
         cursor.execute(f"UPDATE users SET coins = coins + {str(amount)} WHERE id = {str(ctx.author.id)}")
         cursor.execute(f"UPDATE users SET bank = bank - {str(amount)} WHERE id = {str(ctx.author.id)}")
         connect.commit()
-        await ctx.send(embed=discord.Embed(title=f"You withdrew {amount} coins.", color=discord.Color.green()))
+        await ctx.send(embed=discord.Embed(title=f"You withdrew `${addcomma(amount)}` coins.", color=discord.Color.green()))
         
     @commands.command(name="balance", brief="Check your balance", aliases=["bal"])
-    async def bal(self, ctx):
+    async def bal(self, ctx, user: discord.Member = None):
         if connect.is_connected(): pass
         else: connect.reconnect(attempts=3)
-        #Get coins from wallet and display bank balance
-        cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
-        if cursor.fetchone() is None:
-            await ctx.send(embed=discord.Embed(title="You have no coins in your wallet or bank. Broke ass", color=discord.Color.red()))
+        
+        if user is None:
+            #Get coins from wallet and display bank balance
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+            if cursor.fetchone() is None:
+                await ctx.send(embed=discord.Embed(title="You have no coins in your wallet or bank. Broke ass", color=discord.Color.red()))
+                return
+            
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+            coins = cursor.fetchone()[0]
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+            bank = cursor.fetchone()[2]
+            embed = discord.Embed(title="Balance", description=f"**Wallet:** `${addcomma(coins)}`\n**Bank:** `${addcomma(bank)}`", color=discord.Color.green())
+            await ctx.send(embed=embed)
+            connect.commit()
+            return
+        else: 
+            #Get coins from wallet and display bank balance
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(user.id)};")
+            if cursor.fetchone() is None:
+                await ctx.send(embed=discord.Embed(title="This user has no coins in their wallet or bank. Broke ass", color=discord.Color.red()))
+                return
+            
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(user.id)};")
+            coins = cursor.fetchone()[0]
+            cursor.execute(f"SELECT * FROM users WHERE id = {str(user.id)};")
+            bank = cursor.fetchone()[2]
+            embed = discord.Embed(title=f"{user.name}#{user.discriminator}'s Balance", description=f"**Wallet:** `${addcomma(coins)}`\n**Bank:** `${addcomma(bank)}`", color=discord.Color.green())
+            await ctx.send(embed=embed)
+            connect.commit()
+            return
+        
+    @commands.command(name="pay", brief="Pay someone", aliases=["give"])
+    async def pay(self, ctx, user: discord.Member, amount: str = 0):
+        if user.id == ctx.author.id:
+            await ctx.send(embed=discord.Embed(title="You can't pay yourself. Bruh.", color=discord.Color.red()))
+            return
+        
+        if connect.is_connected(): pass
+        else: connect.reconnect(attempts=3)
+        try:
+            if int(amount) == 0:
+                await ctx.send(embed=discord.Embed(title="Please specify an amount of coins to pay.", color=discord.Color.red()))
+                return
+            
+        except:
+            if str(amount) == "all":
+                cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+                if cursor.fetchone() is None:
+                    await ctx.send(embed=discord.Embed(title="You don't have any coins to pay.", color=discord.Color.red()))
+                    return
+                
+                cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+                coins = cursor.fetchone()[0]
+                amount = coins
+                amount = int(amount)
+                tax = round(amount / 100) #Taxes user. If user was to give $900,000, they would be taxed $9,000.
+                amount = (amount - tax) #Removes total for tax
+                amount = (amount + tax) #Adds tax on
+
+        amount = int(amount)
+
+        try:
+            if tax >= 1: pass
+        except:
+            tax = round(amount / 100) #Taxes user. If user was to give $900,000, they would be taxed $9,000.
+            amount = (amount + tax) #Adds tax to amount
+            
+        
+        if amount < 0:
+            await ctx.send(embed=discord.Embed(title="Please specify a positive amount of coins to pay.", color=discord.Color.red()))
             return
         
         cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
-        coins = cursor.fetchone()[0]
-        cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
-        bank = cursor.fetchone()[2]
-        embed = discord.Embed(title="Balance", description=f"**Wallet:** `{coins}`\n**Bank:** `{bank}`", color=discord.Color.green())
-        await ctx.send(embed=embed)
-        connect.commit()
+        if cursor.fetchone() is None:
+            await ctx.send(embed=discord.Embed(title="You don't have any coins to pay. Try depositing some coins", color=discord.Color.red()))
+            return
         
-    #@commands.command(name="pay", brief="Pay someone", aliases=["pay"])
+        cursor.execute(f"SELECT * FROM users WHERE id = {str(ctx.author.id)};")
+        if cursor.fetchone()[0] < amount:
+            await ctx.send(embed=discord.Embed(title=f"You don't have enough coins to pay. `${addcomma(amount - tax)}` (+ `{tax}` tax)", color=discord.Color.red()))
+            return
+        
+        cursor.execute(f"SELECT * FROM users WHERE id = {str(user.id)};")
+        if cursor.fetchone() is None:
+            cursor.execute(f"INSERT INTO users (id, coins, bank) VALUES ({str(user.id)}, {str(0)}, {str(0)});")
+        
+        
+        cursor.execute(f"UPDATE users SET coins = coins - {str(amount)} WHERE id = {str(ctx.author.id)}")
+        cursor.execute(f"UPDATE users SET coins = coins + {str(amount - tax)} WHERE id = {str(user.id)}")
+        connect.commit()
+        embed=discord.Embed(title=f"You paid {user.name}", color=discord.Color.green())
+        embed.add_field(name="Amount", value=f"`${addcomma(amount - tax)}`", inline=True)
+        embed.add_field(name="Tax", value=f"`${addcomma(tax)}`", inline=True)
+        embed.add_field(name="Total", value=f"`${addcomma(amount)}`", inline=True)
+        await ctx.send(embed=embed)
+        
+        
         
         
             
