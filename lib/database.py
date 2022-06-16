dc685f2d07b18fe543a15e5c479719aa625c1320
Aden Viney <adenviney@gmail.com>
2022-06-16 16:01:45 +0100

    simplify

--- lib/database.py
+++ lib/database.py
@@ -4,20 +4,15 @@ import lib.color as c, json, mysql.connector
 class db: 
     def connect(db: str):
         if db == "db1": 
-            with open('./json/database-conf.json') as f: db1 = json.load(f)
-            db = db1
+            with open('./json/database-conf.json') as f: db = json.load(f)
         elif db == "db2": 
-            with open('./json/database-conf2.json') as f: db2 = json.load(f)    
-            db = db2
+            with open('./json/database-conf2.json') as f: db = json.load(f)    
         elif db == "db3": 
-            with open('./json/database-conf3.json') as f: db3 = json.load(f)
-            db = db3
+            with open('./json/database-conf3.json') as f: db = json.load(f)
         elif db == "db4": 
-            with open('./json/database-conf4.json') as f: db4 = json.load(f)
-            db = db4
+            with open('./json/database-conf4.json') as f: db = json.load(f)
         elif db == "db5": 
-            with open('./json/database-conf5.json') as f: db5 = json.load(f)
-            db = db5
+            with open('./json/database-conf5.json') as f: db = json.load(f)
 
         try:
             connect = mysql.connector.connect(**db)
